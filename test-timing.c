ef268979dcaee724005c4c59504a8f8762ea0b37
pct960 <pct960@gmail.com>
2018-08-17 20:57:54 +0530

    Modified algorithm 2^256 possible ciphers

--- test-timing.c
+++ test-timing.c
@@ -28,16 +28,18 @@ int main (int argc, char **argv)
 
 	u16 expected_hash [MSG_LEN/64 + 1];
 
-	u16 min_rounds 	= 8;
-	u16 max_rounds 	= 32;
+	u32 min_rounds 	= 8;
+	u32 max_rounds 	= 32;
 
-	u16 hash_interval   = 1;
+	u32 hash_interval   = 1;
 
 	u8 pepper_bits = 16;
+	u8 num_init_hashes = 7;
 
 	for (i = 0; i < 32; ++i) {
 		key[i] = (u8)arc4random();
 	}
+
 	for (i = 0; i < 12; ++i) {
 		iv[i] = (u8)arc4random();
 	}
@@ -54,9 +56,9 @@ int main (int argc, char **argv)
 			min_rounds,
 			max_rounds,
 			hash_interval,
-			pepper_bits	
+			pepper_bits,
+			num_init_hashes	
 	);
-
 	freestyle_encrypt (&encrypt, message, ciphertext, MSG_LEN, expected_hash);
 
 	clock_gettime(CLOCK_MONOTONIC, &ts_start);
@@ -69,18 +71,23 @@ int main (int argc, char **argv)
 			max_rounds,
 			hash_interval,
 			pepper_bits,
+			num_init_hashes,
 			encrypt.init_hash	
 	);
 	freestyle_decrypt (&decrypt_correct, ciphertext, plaintext, MSG_LEN, expected_hash);
 	clock_gettime(CLOCK_MONOTONIC, &ts_end);
+
+	fflush(stdout);
 
 	assert (0 == memcmp (plaintext, message, MSG_LEN));
 
 	t_kc = (double) (ts_end.tv_nsec - ts_start.tv_nsec) +
             (double) (ts_end.tv_sec - ts_start.tv_sec)*1000000000;
 
+	printf("------------------------------------------------------------------------------------------------\n");
+	printf("[ Timing tests ]\n");
+	printf("------------------------------------------------------------------------------------------------\n");
 	printf ("Time taken to decrypt using the CORRECT key                      = %f nano seconds\n",t_kc);
-	fflush(stdout);
 
 	clock_gettime(CLOCK_MONOTONIC, &ts_start);
 	freestyle_init_decrypt_with_pepper (
@@ -92,6 +99,7 @@ int main (int argc, char **argv)
 			max_rounds,
 			hash_interval,
 			pepper_bits,
+			num_init_hashes,
 			encrypt.pepper,
 			encrypt.init_hash
 	);
@@ -115,6 +123,7 @@ int main (int argc, char **argv)
 			max_rounds,
 			hash_interval,
 			pepper_bits,
+			num_init_hashes,
 			encrypt.init_hash	
 	);
 	clock_gettime(CLOCK_MONOTONIC, &ts_end);
@@ -123,11 +132,13 @@ int main (int argc, char **argv)
 	t_kw = (double) (ts_end.tv_nsec - ts_start.tv_nsec) +
             (double) (ts_end.tv_sec - ts_start.tv_sec)*1000000000;
 
-	printf ("Time taken to attempt decryption using the WRONG key             = %f nano seconds\n",t_kw);
+	printf("Time taken to attempt decryption using the WRONG key             = %f nano seconds\n",t_kw);
 	printf("Key guessing penalty (using uniform random number generator)     = %f\n",t_kw/t_kc);
 
-	printf("\n\n");
-
-	printf ("Time taken to decrypt using the CORRECT key (using known pepper) = %f nano seconds\n",t_kp);
+	printf("------------------------------------------------------------------------------------------------\n");
+	printf("Time taken to decrypt using the CORRECT key (using known pepper) = %f nano seconds\n",t_kp);
 	printf("Key guessing penalty (when the pepper is known to the reciever)  = %f\n",t_kw/t_kp);
+	printf("------------------------------------------------------------------------------------------------\n");
+
+	return 0;
 }
